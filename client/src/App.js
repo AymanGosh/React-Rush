865da55280793a62eaa11d4b5bbbc2de08f1509d
aymangosh <aymansce@gmail.com>
2025-08-04 22:33:48 +0300

    Set up client-side routing for the app using react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
  
+import Landing from "./components/Landing";
 import HomePage from "./components/HomePage";
+import { Routes, Route } from 'react-router-dom'; 
 
 export default function App() {
  
-  return (<>
-    <HomePage/>
-  </>)
+  return (
+    <Routes>
+      <Route path="/" element={<Landing  />} />
+      <Route path="/question1" element={ <HomePage/>} />
+    </Routes>
+  
+ 
+   
+  )
 }
